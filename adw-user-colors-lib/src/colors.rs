345540365a5701969608e2dbde91cd4be602bb95
Ashley Wulber <ashley@system76.com>
2022-06-13 14:03:13 -0400

    derive default

--- adw-user-colors-lib/src/colors.rs
+++ adw-user-colors-lib/src/colors.rs
@@ -2,7 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Default, Deserialize, Serialize, Clone)]
 pub struct ColorOverrides {
     /// name
     pub name: String,
